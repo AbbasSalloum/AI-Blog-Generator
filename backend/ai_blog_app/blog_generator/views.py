85f44cbaebc9b402f3e93af194d15d249e5df8df
AbbasSalloum <Salloum916@gmail.com>
2024-05-08 12:31:55 -0400

    update to views.py

--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
-from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
@@ -11,18 +10,16 @@ from pytube import YouTube
 import os
 import assemblyai as aai
 import openai
-
 import environ  
 
 env = environ.Env()
 environ.Env.read_env()
+# from .models import BlogPost
+
 # Create your views here.
-
-
 @login_required
 def index(request):
     return render(request, 'index.html')
-
 
 @csrf_exempt
 def generate_blog(request):
@@ -30,27 +27,37 @@ def generate_blog(request):
         try:
             data = json.loads(request.body)
             yt_link = data['link']
-        except(KeyError, json.JSONDecodeError):
-            return JsonResponse({'error': 'Invalid data sent'}, status = 400)
+        except (KeyError, json.JSONDecodeError):
+            return JsonResponse({'error': 'Invalid data sent'}, status=400)
+
+
         # get yt title
         title = yt_title(yt_link)
 
         # get transcript
-        transcription  = get_transcription(yt_link)
+        transcription = get_transcription(yt_link)
         if not transcription:
-            return JsonResponse({'error': " Failed to get transcript"}, status = 400)
+            return JsonResponse({'error': " Failed to get transcript"}, status=500)
 
-        # use openAI to generate the blog
+
+        # use OpenAI to generate the blog
         blog_content = generate_blog_from_transcription(transcription)
         if not blog_content:
-            return JsonResponse({'error' : 'Failed to generate blog article'}, status = 500)
+            return JsonResponse({'error': " Failed to generate blog article"}, status=500)
 
         # save blog article to database
+        # new_blog_article = BlogPost.objects.create(
+        #     user=request.user,
+        #     youtube_title=title,
+        #     youtube_link=yt_link,
+        #     generated_content=blog_content,
+        # )
+        # new_blog_article.save()
 
         # return blog article as a response
         return JsonResponse({'content': blog_content})
     else:
-        return JsonResponse({'error': 'Invalid Request Method'}, status = 405)
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 def yt_title(link):
     yt = YouTube(link)
@@ -59,7 +66,7 @@ def yt_title(link):
 
 def download_audio(link):
     yt = YouTube(link)
-    video = yt.streams.filter(only_audio= True).first()
+    video = yt.streams.filter(only_audio=True).first()
     out_file = video.download(output_path=settings.MEDIA_ROOT)
     base, ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
@@ -75,7 +82,33 @@ def get_transcription(link):
 
     return transcript.text
 
-    
+def generate_blog_from_transcription(transcription):
+    openai.api_key = env("OPENAI_KEY")
+
+    prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but dont make it look like a youtube video, make it look like a proper blog article:\n\n{transcription}\n\nArticle:"
+
+    response = openai.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=prompt,
+        max_tokens=250
+    )
+
+    generated_content = response.choices[0].text.strip()
+
+    return generated_content
+
+
+
+# def blog_list(request):
+#     blog_articles = BlogPost.objects.filter(user=request.user)
+#     return render(request, "all-blogs.html", {'blog_articles': blog_articles})
+
+# def blog_details(request, pk):
+#     blog_article_detail = BlogPost.objects.get(id=pk)
+#     if request.user == blog_article_detail.user:
+#         return render(request, 'blog-details.html', {'blog_article_detail': blog_article_detail})
+#     else:
+#         return redirect('/')
 
 def user_login(request):
     if request.method == 'POST':
@@ -89,23 +122,8 @@ def user_login(request):
         else:
             error_message = "Invalid username or password"
             return render(request, 'login.html', {'error_message': error_message})
+        
     return render(request, 'login.html')
-
-
-def generate_blog_from_transcription(transcription):
-    openai.api_key = env("OPENAI_KEY")
-
-    prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but dont make it look like a youtube video, make it look like a proper blog article:\n\n{transcription}\n\nArticle:"
-
-    response = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt = prompt,
-        max_tokens = 1000
-    )
-
-    generated_content = response.choices[0].text.strip()
-
-    return generated_content
 
 def user_signup(request):
     if request.method == 'POST':
@@ -122,11 +140,12 @@ def user_signup(request):
                 return redirect('/')
             except:
                 error_message = 'Error creating account'
-                return render(request, 'signup.html', {'error_message' : error_message})
+                return render(request, 'signup.html', {'error_message':error_message})
         else:
             error_message = 'Password do not match'
-            return render(request, 'signup.html', {'error_message' : error_message})
-    return render(request, 'Signup.html')
+            return render(request, 'signup.html', {'error_message':error_message})
+        
+    return render(request, 'signup.html')
 
 def user_logout(request):
     logout(request)
